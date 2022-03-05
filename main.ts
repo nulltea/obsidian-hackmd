2d0b114ed18ac6d609b7dd14af55a4dfdeabaab2
Timofey Luin <timofey.luin@gmail.com>
2022-03-06 01:09:26 +0200

    impl dumb publish

--- main.ts
+++ main.ts
@@ -1,72 +1,69 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	LinkCache,
+	request,
+	ToggleComponent, TFile
+} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface ObsidianHackMDSettings {
+	hackMdApiToken: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ObsidianHackMDSettings = {
+	hackMdApiToken: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const baseURL: string = "https://api.hackmd.io"
+
+export default class ObsidianHackMD extends Plugin {
+	settings: ObsidianHackMDSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const ribbonIconEl = this.addRibbonIcon('paper-plane', 'Publish to HackMD', async (evt: MouseEvent) => {
+			let resp = JSON.parse(await request({
+				method: "POST",
+				url: new URL('/v1/notes', baseURL).href,
+				body: JSON.stringify({
+					"title": this.app.workspace.getActiveFile().name,
+					"content": await this.app.vault.read(this.app.workspace.getActiveFile()),
+					"readPermission": "owner",
+					"writePermission": "owner",
+					"commentPermission": "everyone"
+				}),
+				headers: {
+					'Authorization': 'Bearer ' + this.settings.hackMdApiToken,
+					"Content-Type": "application/json"
+				},
+			}));
+
+			console.log(resp)
+
+			if (resp.error !== undefined) {
+				return false;
+			} else {
+				new Notice("Published!", 3600)
+			}
 		});
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new ObsidianHackMDSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -91,14 +88,46 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+type LinkedNotesSelectorCallback = (selected: Map<TFile, boolean>) => void;
+
+class LinkedNotesSelector extends Modal {
+	links: LinkCache[];
+	onSelect: LinkedNotesSelectorCallback;
+	_toggleMap: Map<TFile, ToggleComponent>;
+
+	constructor(app: App, links: LinkCache[], onSelect: LinkedNotesSelectorCallback) {
 		super(app);
+		this.links = links;
+		this._toggleMap = new Map<TFile, ToggleComponent>();
+		this.onSelect = onSelect;
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.createEl('h3', {text: 'Export linked notes?'});
+		contentEl.createSpan({text: 'It looks like this note contains outgoing links. Select which of the linked notes to publish along with current'});
+
+		this.links.forEach(link => {
+			let file = this.app.metadataCache.getFirstLinkpathDest(link.link, this.app.workspace.getActiveFile().path);
+			new Setting(contentEl)
+				.setName(link.displayText)
+				.setDesc(`${link.position.end.line}:${link.position.end.col}: ${link.original}`)
+				.addToggle(t => {
+					t.setValue(true);
+					this._toggleMap.set(file, t);
+				})
+		})
+
+		new Setting(contentEl).addButton(b => {
+			b.setIcon("enter").setButtonText("CONFIRM").onClick(evt => {
+				let selected = new Map<TFile, boolean>();
+				this._toggleMap.forEach((v, l, _) => {
+					selected.set(l, v.getValue());
+				})
+				this.onSelect(selected);
+				this.close();
+			})
+		})
 	}
 
 	onClose() {
@@ -107,10 +136,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class ObsidianHackMDSettingTab extends PluginSettingTab {
+	plugin: ObsidianHackMD;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ObsidianHackMD) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -119,19 +148,47 @@ class SampleSettingTab extends PluginSettingTab {
 		const {containerEl} = this;
 
 		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h3', {text: 'Publish to HackMD - Settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('API Token')
+			.setDesc('Your HackMD API Token. Create new here: https://hackmd.io/settings#api')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Paste token here')
+				.setValue(this.plugin.settings.hackMdApiToken)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.hackMdApiToken = value;
 					await this.plugin.saveSettings();
 				}));
 	}
+}
+
+async function publishFiles(files: TFile[]) : Promise<boolean> {
+	for (let file of files) {
+		let resp = JSON.parse(await request({
+			method: "POST",
+			url: new URL('/v1/notes', baseURL).href,
+			body: JSON.stringify({
+				"title": this.app.workspace.getActiveFile().name,
+				"content": await this.app.vault.read(this.app.workspace.getActiveFile()),
+				"readPermission": "owner",
+				"writePermission": "owner",
+				"commentPermission": "everyone"
+			}),
+			headers: {
+				'Authorization': 'Bearer ' + this.settings.hackMdApiToken,
+				"Content-Type": "application/json"
+			},
+		}));
+
+		console.log(resp)
+
+		if (resp.error !== undefined) {
+			return false;
+		} else {
+			new Notice("Published!", 3600)
+		}
+	}
+
+	return true;
 }
